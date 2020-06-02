89efc3135623a991504d8cdfd8ceff6d163da452
Prima Wirawan <primailkom@gmail.com>
2020-06-02 19:48:15 +0700

    Add simple variable to render

--- src/App.js
+++ src/App.js
@@ -2,9 +2,17 @@ import React from 'react';
 import './App.css';
 
 function App() {
+  const counter = 3;
+
+  const hello = () => {
+    console.log("hello world!")
+  }
+
   return (
     <div>
       <h1>Hello React!</h1>
+      <button>{counter}</button>
+      <button onClick={hello}>Show Hello</button>
     </div>
   );
 }
