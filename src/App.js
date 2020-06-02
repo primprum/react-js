2573e24180f24cb7117662cde106e0ba085fa6bf
Prima Wirawan <primailkom@gmail.com>
2020-06-02 21:50:17 +0700

    Giving comments and moving the Tweet file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import './App.css';
-import Tweet from './Tweet';
+import Tweet from './components/Tweet';
 
 function App() {
   const counter = 3;
@@ -21,7 +21,7 @@ function App() {
   const [users] = useState([
     { name: "Ed", message: "Hello There!" },
     { name: "John", message: "Hello I am John!" },
-    { name: "Traversy", message: "Hello I am awesome!" }
+    { name: "Traversy", message: "Hello I am awesome!" } 
   ]);
 
   return (
@@ -30,16 +30,19 @@ function App() {
       <button>{counter}</button>
       <button onClick={hello}>Show Hello</button>
 
-      <Tweet name="John" message="Good morning" likes={400}/>
-      <Tweet name="Michael" message="Good afternoon" likes={500}/>
-      <Tweet name="Adam" message="Good evening" likes={700}/>
-
+      {/* increment value and flipping color */}
       <button onClick={increment}>Increment</button>
       <h1>{count}</h1>
-      <h1 className={isRed ? 'red' : ''}>This color should changed!</h1>
+      <h1 className={isRed ? 'red' : ''}>This color should changed!</h1> 
 
-      {users.map(user => (
-        <Tweet name={user.name} message={user.message}/>
+      {/* manual assign */}
+      <Tweet name="John" message="Good morning" likes={400}/>
+      <Tweet name="Michael" message="Good afternoon" likes={500}/> 
+      <Tweet name="Adam" message="Good evening" likes={700}/>
+
+      {/* using map */}
+      {users.map(user => ( 
+        <Tweet name={user.name} message={user.message}/> // using format from Tweet
       ))}
     </div>
   );
