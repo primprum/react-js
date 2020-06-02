984a64f057416740519771376e684ac0db96df9c
Prima Wirawan <primailkom@gmail.com>
2020-06-02 20:12:25 +0700

    Create Tweet component

---
+++ src/Tweet.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Tweet() {
+    return(
+        <div>
+            <h3>Name</h3>
+            <p>This is random tweet</p>
+            <h3>Total of likes: </h3>
+        </div>
+    );
+}
+
+export default Tweet;
