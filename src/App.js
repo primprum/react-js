e2b66dfe56d5f27b30a8a65cce594733c2fb4eee
Prima Wirawan <primailkom@gmail.com>
2021-12-05 16:03:40 +0700

    learn on how to use useState

--- src/App.js
+++ src/App.js
@@ -1,50 +1,23 @@
-import React, { useState } from 'react';
-import './App.css';
-import Tweet from './components/Tweet';
+// learn on how to use useState
+import React, { useState } from "react";
 
 function App() {
-  const counter = 3;
-
-  const hello = () => {
-    console.log("hello world!")
-  }
-
-  const [isRed, setRed] = useState(false);
   const [count, setCount] = useState(0);
 
-  const increment = () => {
-    setCount(count + 1);
-    // setRed(true);
-    setRed(!isRed) // flip the state
+  function decrementCount() {
+    setCount(count - 1);
   }
 
-  const [users] = useState([
-    { name: "Ed", message: "Hello There!" },
-    { name: "John", message: "Hello I am John!" },
-    { name: "Traversy", message: "Hello I am awesome!" } 
-  ]);
+  function incrementCount() {
+    setCount(count + 1);
+  }
 
   return (
-    <div className="my-tweets">
-      <h1>Hello React!</h1>
-      <button>{counter}</button>
-      <button onClick={hello}>Show Hello</button>
-
-      {/* increment value and flipping color */}
-      <button onClick={increment}>Increment</button>
-      <h1>{count}</h1>
-      <h1 className={isRed ? 'red' : ''}>This color should changed!</h1> 
-
-      {/* manual assign */}
-      <Tweet name="John" message="Good morning" likes={400}/>
-      <Tweet name="Michael" message="Good afternoon" likes={500}/> 
-      <Tweet name="Adam" message="Good evening" likes={700}/>
-
-      {/* using map */}
-      {users.map(user => ( 
-        <Tweet name={user.name} message={user.message}/> // using format from Tweet
-      ))}
-    </div>
+    <>
+      <button onClick={decrementCount}>-</button>
+      <span>{count}</span>
+      <button onClick={incrementCount}>+</button>
+    </>
   );
 }
 
---
+++ src/App1.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import './App.css';
+import Tweet from './components/Tweet';
+
+function App() {
+  const counter = 3;
+
+  const hello = () => {
+    console.log("hello world!")
+  }
+
+  const [isRed, setRed] = useState(false);
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+    // setRed(true);
+    setRed(!isRed) // flip the state
+  }
+
+  const [users] = useState([
+    { name: "Ed", message: "Hello There!" },
+    { name: "John", message: "Hello I am John!" },
+    { name: "Traversy", message: "Hello I am awesome!" } 
+  ]);
+
+  return (
+    <div className="my-tweets">
+      <h1>Hello React!</h1>
+      <button>{counter}</button>
+      <button onClick={hello}>Show Hello</button>
+
+      {/* increment value and flipping color */}
+      <button onClick={increment}>Increment</button>
+      <h1>{count}</h1>
+      <h1 className={isRed ? 'red' : ''}>This color should changed!</h1> 
+
+      {/* manual assign */}
+      <Tweet name="John" message="Good morning" likes={400}/>
+      <Tweet name="Michael" message="Good afternoon" likes={500}/> 
+      <Tweet name="Adam" message="Good evening" likes={700}/>
+
+      {/* using map */}
+      {users.map(user => ( 
+        <Tweet name={user.name} message={user.message}/> // using format from Tweet
+      ))}
+    </div>
+  );
+}
+
+export default App;
