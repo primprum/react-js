c7461739c2624c8c9c2b5ee0486f7b7e8ccfb232
Prima Wirawan <primailkom@gmail.com>
2021-12-05 16:17:42 +0700

    better setCount approach

--- src/App.js
+++ src/App.js
@@ -4,19 +4,46 @@ import React, { useState } from "react";
 function App() {
   const [count, setCount] = useState(0);
 
-  function decrementCount() {
-    setCount(count - 1);
+  // function decrementCount() {
+  //   setCount(count - 1);
+  // }
+
+  // function incrementCount() {
+  //   setCount(count + 1);
+  // }
+
+  // the problem about using that setCount style above is that if it's being like below:
+
+  // function decrementCount() {
+  //   setCount(count - 1);
+  //  setCount(count - 1);
+  // }
+
+  // then it will not reduce the value by 2 decrement
+  // so, would be better if like this:
+
+  function betterDecrementCount() {
+    setCount((prevCount) => prevCount - 1);
   }
 
-  function incrementCount() {
-    setCount(count + 1);
+  function betterIncrementCount() {
+    setCount((prevCount) => prevCount + 1);
   }
+
+  // so, if like this:
+
+  // function betterDecrementCount() {
+  //   setCount((prevCount) => prevCount - 1);
+  //   setCount((prevCount) => prevCount - 1);
+  // }
+
+  // then it will be decreased by 2
 
   return (
     <>
-      <button onClick={decrementCount}>-</button>
+      <button onClick={betterDecrementCount}>-</button>
       <span>{count}</span>
-      <button onClick={incrementCount}>+</button>
+      <button onClick={betterIncrementCount}>+</button>
     </>
   );
 }
