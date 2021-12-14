3a82086a83b75c6cd00db2c76ed66d23bb800b78
Prima Wirawan <primailkom@gmail.com>
2021-12-14 17:27:06 +0700

    show map into page

--- src/components/MapFunc.js
+++ src/components/MapFunc.js
@@ -3,12 +3,14 @@ import React from "react";
 const MapFunc = () => {
   const companies = [
     {
+      id: 1,
       name: "Company One",
       category: "Finance",
       start: 1981,
       end: 2003,
     },
     {
+      id: 2,
       name: "Company Two",
       category: "Retail",
       start: 1992,
@@ -22,12 +24,18 @@ const MapFunc = () => {
 
   return (
     <>
+      {/* this is somehow incorrect, only making it on the same line */}
       {/* <h1>{companiesName}</h1> */}
 
       {/* directly, without making function variable first */}
       {companies.map((company) => {
         return console.log(company.name);
       })}
+
+      {/* best way to show it into the page */}
+      {companies.map((company) => (
+        <h1>{company.name}</h1>
+      ))}
     </>
   );
 };
